0a305c8d10c9fa492209cbb7206727dc10abfdd8
aojha@Shyam <aojha@gmail.com>
2024-09-08 15:40:35 +0530

    Ui created

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./rootes/App.jsx";
+import Bag from "./rootes/Bag.jsx";
+import "./index.css";
+import Home from "./rootes/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
